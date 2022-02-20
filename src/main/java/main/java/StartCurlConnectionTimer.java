c0602c1b5e538390ffd0ab7dc1af8eb932e16290
Oleg Akulov <akulov.oleg@mail.ru>
2022-02-20 16:46:29 +0300

    add correct date

--- src/main/java/main/java/StartCurlConnectionTimer.java
+++ src/main/java/main/java/StartCurlConnectionTimer.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.TimerTask;
 import java.util.Timer;
 
@@ -29,12 +30,16 @@ public class StartCurlConnectionTimer extends TimerTask {
     }
 
 
-    public  void runTimer() {
+    public void runTimer() {
         Calendar startDataTime = Calendar.getInstance();
-        startDataTime.set(2022, 1, 19, 17, 29);
+        startDataTime.set(2022, 1, 21, 17, 29);
         Calendar currentTime = Calendar.getInstance();
+        Date date = new Date();
         TimerTask timerTask = new StartCurlConnectionTimer();
         Timer timer = new Timer();
+        if (currentTime.getTime().compareTo(startDataTime.getTime()) < 0) {
+            startDataTime.set(2022, date.getMonth(), date.getDay(), 17, 29);
+        }
         timer.scheduleAtFixedRate(timerTask, startDataTime.getTimeInMillis() - currentTime.getTimeInMillis(), 24 * 60 * 60 * 1000);
     }
 }
